a8bbc0d05478307404e3a547f3519da921a2e7f6
kimdaeyoon <89075605+apeachicetea@users.noreply.github.com>
2021-11-28 12:19:33 +0900

    Redux

---
+++ src/A.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function A () {
+
+
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default A;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import A from './A';
+
+
+
+
+function App() {
+  return null;
+}
+
+export default App;
